381315f75b39a71e99ba800f31b6e9fbd5ec3b1b
Michael F. Weinberg <mweinberg@seatgeek.com>
2019-04-02 11:22:53 -0700

    Accept defaults for all flags from environment variables

--- lib/stack-profile/command.rb
+++ lib/stack-profile/command.rb
@@ -6,86 +6,120 @@ module Sfn
    class Validate
       attribute(
         :profile, String,
-        :description => 'Stack Profile'
+        :description => 'Stack Profile',
+        :default => ENV['PROFILE']
       )
       attribute(
         :environment, String,
-        :description => 'Profile Environment'
+        :description => 'Profile Environment',
+        :default => ENV['ENVIRONMENT']
       )
       attribute(
         :role, String,
-        :description => 'Profile Role'
+        :description => 'Profile Role',
+        :default => ENV['ROLE']
       )
       attribute(
         :version, String,
-        :description => 'Stack Version'
+        :description => 'Stack Version',
+        :default => ENV['VERSION']
+      )
+      attribute(
+      :defaults, [TrueClass, FalseClass],
+      :default => ENV['DEFAULTS'] == 'true' ? true : false
       )
    end
    class Create
      attribute(
        :profile, String,
-       :description => 'Stack Profile'
+       :description => 'Stack Profile',
+       :default => ENV['PROFILE']
      )
      attribute(
        :environment, String,
-       :description => 'Profile Environment'
+       :description => 'Profile Environment',
+       :default => ENV['ENVIRONMENT']
      )
      attribute(
        :role, String,
-       :description => 'Profile Role'
-     )
-     attribute(
-       :ttl, String,
-       :description => 'Stack TTL'
+       :description => 'Profile Role',
+       :default => ENV['ROLE']
      )
      attribute(
        :version, String,
-       :description => 'Stack Version'
+       :description => 'Stack Version',
+       :default => ENV['VERSION']
+     )
+     attribute(
+       :ttl, String,
+       :description => 'Stack TTL',
+       :default => ENV['TTL']
+     )
+     attribute(
+       :defaults, [TrueClass, FalseClass],
+       :default => ENV['DEFAULTS'] == 'true' ? true : false
      )
    end
-   ## Update and Change Sets don't work yet
    class Update
      attribute(
        :profile, String,
-       :description => 'Stack Profile'
+       :description => 'Stack Profile',
+       :default => ENV['PROFILE']
      )
      attribute(
        :environment, String,
-       :description => 'Profile Environment'
+       :description => 'Profile Environment',
+       :default => ENV['ENVIRONMENT']
      )
      attribute(
        :role, String,
-       :description => 'Profile Role'
-     )
-     attribute(
-       :ttl, String,
-       :description => 'Stack TTL'
+       :description => 'Profile Role',
+       :default => ENV['ROLE']
      )
      attribute(
        :version, String,
-       :description => 'Stack Version'
+       :description => 'Stack Version',
+       :default => ENV['VERSION']
+     )
+     attribute(
+       :ttl, String,
+       :description => 'Stack TTL',
+       :default => ENV['TTL']
+     )
+     attribute(
+       :defaults, [TrueClass, FalseClass],
+       :default => ENV['DEFAULTS'] == 'true' ? true : false
      )
    end
    class ChangeSet
      attribute(
        :profile, String,
-       :description => 'Stack Profile'
+       :description => 'Stack Profile',
+       :default => ENV['PROFILE']
      )
      attribute(
        :environment, String,
-       :description => 'Profile Environment'
+       :description => 'Profile Environment',
+       :default => ENV['ENVIRONMENT']
      )
      attribute(
        :role, String,
-       :description => 'Profile Role'
-     )
-     attribute(
-       :ttl, String,
-       :description => 'Stack TTL'
+       :description => 'Profile Role',
+       :default => ENV['ROLE']
      )
      attribute(
        :version, String,
-       :description => 'Stack Version'
+       :description => 'Stack Version',
+       :default => ENV['VERSION']
+     )
+     attribute(
+       :ttl, String,
+       :description => 'Stack TTL',
+       :default => ENV['TTL']
+     )
+     attribute(
+       :defaults, [TrueClass, FalseClass],
+       :default => ENV['DEFAULTS'] == 'true' ? true : false
      )
    end
  end
