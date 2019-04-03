d8ecee29a608fbc4c84650d966411738f64df553
Michael F. Weinberg <mweinberg@seatgeek.com>
2019-04-03 16:20:22 -0700

    Experimenting to get RSpec loading correctly

--- lib/stack-profile.rb
+++ lib/stack-profile.rb
@@ -1,3 +1,3 @@
 require 'stack-profile/version'
 require 'stack-profile/profile'
-require 'stack-profile/command'
+#require 'stack-profile/command'
--- spec/merge_spec.rb
+++ spec/merge_spec.rb
@@ -1,24 +1,28 @@
 require 'bogo-config'
 require 'sfn'
 
-require 'stack-profile'
+#require 'stack-profile'
+#require 'stack-profile/command'
+require 'stack-profile/profile'
 
 RSpec.describe Sfn::Callback::StackProfileCallback do
+  let(:ui) { double("ui") }
+  let(:config) { Smash.new }
+  let(:arguments) { [] }
 
-  context = {
-    stack_profile: {
-      profile_directory: 'spec',
-      allowed_values: {}
-    },
-    environment: 'env1',
-    role: 'role',
-    version: 'v0.0.0'
-  }
+  context = {}
+  #   environment: 'env1',
+  #   role: 'role',
+  #   version: 'v0.0.0'
+  # }
 
   profile = Sfn::Callback::StackProfileCallback.new('profile_spec', context, 'test-stack', true)
 
-  profile.profile_config('profile_spec', context, 'test-stack')
-  # profile.profile_config
+
+  profile.load_config('profile_spec')
+
+  profile.profile_config('profile_spec', context)
+  profile.profile_config
 
   puts profile
   it "has config" do
